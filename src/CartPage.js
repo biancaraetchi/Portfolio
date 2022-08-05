53d9c777d47663c0c96c4ed3992579762e6d9a34
Bianca Raetchi <bianca.raetchi@gmail.com>
2022-08-05 14:16:34 +0300

    Finished and opmtimized mobile responsiveness for Cartpage

--- src/CartPage.js
+++ src/CartPage.js
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom';
 
 const CartPage = (props) => {
     useEffect(() => {
-    }, [])
+    }, []);
+    var total = 0;
+    props.lamps.map((lamp) =>{
+        total += lamp.items*lamp.price;
+    })
+
     return (
         <div className="flexWrapper">
             <div className="formBlock">
                 <div className="allCart">
                     <div className="allItems">
                         <h3>The following items are in your cart:</h3>
- 
-                        {props.lamps.map((lamp) => {
-                            let ifDisplayed = { display: "none" };
-                            if (lamp.items !== 0) {
-                                ifDisplayed.display = "flex";
-                            }
 
+                        {props.lamps.map((lamp) => {
                             return (
-                                <div className="item" key={lamp.id} style={ifDisplayed}>
+                                lamp.items !== 0 && <div className="item" key={lamp.id} >
                                     <div className="image">
-                                        <img src={"./"+lamp.backimage} alt="f" />
+                                        <img src={"./" + lamp.backimage} alt="f" />
                                     </div>
                                     <div className="text">
                                         <h4>{lamp.title}</h4>
@@ -29,13 +29,25 @@ const CartPage = (props) => {
                                             {lamp.prop1 ? <li>LED</li> : <li>Not LED</li>}
                                             <li>Colors available: {lamp.prop2}</li>
                                         </ul>
-                                        <p>Price: €{lamp.price} x {lamp.items} = €{lamp.price * lamp.items}</p>                                    </div>
+                                        <p>Price: €{lamp.price} x {lamp.items} = <span className="underline">€{(lamp.price * lamp.items).toFixed(2)}</span></p>                                    </div>
                                 </div>
                             )
                         })}
                     </div>
                     <div className="totalCheckout">
                         <h2>Total to checkout:</h2>
+                        <ul className='haha'>
+                            {
+                                props.lamps.map((lamp) => {
+                                    return(
+                                    lamp.items !== 0 && <li key={lamp.id}><i>{lamp.title}</i> x {lamp.items} = <span className="underline">€{(lamp.price * lamp.items).toFixed(2)}</span></li> 
+                                )})
+                            }
+                        </ul>
+                        <p>Total to pay: <span className="underline">€{total.toFixed(2)}</span></p>
+                        <div className="flex-wrapper">
+                        <Link to="/Portfolio/payment"><button>Proceed to payment</button></Link>
+                        </div>
                     </div>
                 </div>
             </div>
