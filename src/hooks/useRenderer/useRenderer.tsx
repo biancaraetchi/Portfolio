1fdd9d3595c6d7d3e4caf1a08e01ecc5f44cb9ac
biancaraetchi <mold.academ@gmail.com>
2024-04-03 10:03:09 +0200

    fix: removing three.js

--- src/hooks/useRenderer/useRenderer.tsx
+++ src/hooks/useRenderer/useRenderer.tsx
@@ -1,34 +1,7 @@
-import * as THREE from 'three';
 import useWindowSize from '../useWindowSize/useWindowSize';
+import React from 'react';
 
 const useRenderer = (canvas: HTMLCanvasElement) => {
-  const { width, height } = useWindowSize()
-  if (canvas) {
-    //Scene
-    const scene = new THREE.Scene()
-    const geometry = new THREE.SphereGeometry(3, 64, 64)
-    const material = new THREE.MeshStandardMaterial({
-      color: "#FF0000"
-    })
-    const mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
-
-    //Light
-    const light = new THREE.PointLight(0xFF00FF, 200, 100)
-    light.castShadow = true
-    light.position.set(-10, 10, 10)
-    scene.add(light)
-
-    //Camera
-    const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100)
-    camera.position.z = 10
-    scene.add(camera)
-
-    //Renderer
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas })
-    renderer.setSize(width, height)
-    renderer.render(scene, camera)
-  }
 }
 
 export default useRenderer;
